b9c8af7afe4a163582dc76b2909bdaa9461b4583
Noah Wagner <wagnernoah@icloud.com>
2025-01-29 18:29:06 -0800

    Edit code style to match rest of project

--- src/components/DetailsBox/DetailsBox_StateSelection.tsx
+++ src/components/DetailsBox/DetailsBox_StateSelection.tsx
@@ -22,37 +22,37 @@ interface DetailsBox_StateSelectionProps {
  * @returns
  */
 export default function DetailsBox_StateSelection(props: DetailsBox_StateSelectionProps) {
-    const { nodeWrappers, startNode, setStartNode } = props;
+  const { nodeWrappers, startNode, setStartNode } = props;
 
-    const nw1 = nodeWrappers[0];
+  const nw1 = nodeWrappers[0];
 
-    const isMultiSelection = nodeWrappers.length > 1;
-    const allAccept = nodeWrappers.every(node => node.isAcceptNode);
-    const noneAccept = nodeWrappers.every(node => !node.isAcceptNode);
+  const isMultiSelection = nodeWrappers.length > 1;
+  const allAccept = nodeWrappers.every(node => node.isAcceptNode);
+  const noneAccept = nodeWrappers.every(node => !node.isAcceptNode);
 
-    const [nodeLabelText, setLabelText] = useState(nw1.labelText);
-    const [isAccept, setIsAccept] = useState(noneAccept ? false : (allAccept || undefined));
-    const [isStartNodeInternal, setIsStartNodeInternal] = useState(StateManager.startNode === nw1);
+  const [nodeLabelText, setLabelText] = useState(nw1.labelText);
+  const [isAccept, setIsAccept] = useState(noneAccept ? false : (allAccept || undefined));
+  const [isStartNodeInternal, setIsStartNodeInternal] = useState(StateManager.startNode === nw1);
 
-    let updateNodeName = (newName: string) => {
-        setLabelText(newName);
-        StateManager.setNodeName(nw1, newName);
-    };
+  let updateNodeName = (newName: string) => {
+    setLabelText(newName);
+    StateManager.setNodeName(nw1, newName);
+  };
 
-    let updateNodeIsAccept = (isAccept: boolean) => {
-        setIsAccept(isAccept);
-        nodeWrappers.forEach(node => {
-            if (node.isAcceptNode !== isAccept)
-                StateManager.setNodeIsAccept(node, isAccept)
-        });
-    };
+  let updateNodeIsAccept = (isAccept: boolean) => {
+    setIsAccept(isAccept);
+    nodeWrappers.forEach(node => {
+      if (node.isAcceptNode !== isAccept)
+        StateManager.setNodeIsAccept(node, isAccept)
+    });
+  };
 
-    const [_, currentStackLocation] = useActionStack();
-    useEffect(() => {
-        setLabelText(nw1.labelText);
-        setIsAccept(noneAccept ? false : (allAccept || undefined));
-        setIsStartNodeInternal(StateManager.startNode === nw1);
-    }, [currentStackLocation]);
+  const [_, currentStackLocation] = useActionStack();
+  useEffect(() => {
+    setLabelText(nw1.labelText);
+    setIsAccept(noneAccept ? false : (allAccept || undefined));
+    setIsStartNodeInternal(StateManager.startNode === nw1);
+  }, [currentStackLocation]);
 
   let nodeNameInput = (
     <input
@@ -60,40 +60,39 @@ export default function DetailsBox_StateSelection(props: DetailsBox_StateSelecti
       type="text"
       placeholder="State name"
       value={nodeLabelText}
-      onChange={(e) => updateNodeName(e.target.value)}
-    ></input>
+      onChange={(e) => updateNodeName(e.target.value)}></input>
   );
-    let nodeAcceptInput = (
-        <input
-            type="checkbox"
-            id="is-accept-state"
-            name="is-accept-state"
-            checked={isAccept}
-            ref={input => {
-                if (input) {
-                    input.indeterminate = isAccept === undefined;
-                }
-            }}
-            onChange={e => updateNodeIsAccept(e.target.checked)}></input>
-    );
+  let nodeAcceptInput = (
+    <input
+      type="checkbox"
+      id="is-accept-state"
+      name="is-accept-state"
+      checked={isAccept}
+      ref={input => {
+        if (input) {
+          input.indeterminate = isAccept === undefined;
+        }
+      }}
+      onChange={e => updateNodeIsAccept(e.target.checked)}></input>
+  );
 
-    let startStateClasses = `${isStartNodeInternal ? 'text-gray-700 dark:text-gray-300' : 'text-blue-500 dark:text-blue-400 '} flex flex-row items-center`
-    return (
-        <div className="flex flex-col">
-            <div className="font-medium text-2xl mb-2">State</div>
-            <div className="divide-y mb-3">
-                {!isMultiSelection && <ListItem title="Name" rightContent={nodeNameInput} />}
-                <ListItem title="Accepts" rightContent={nodeAcceptInput} />
-            </div>
-            {!isMultiSelection && <div className="divide-y mb-3">
-                <CoreListItem>
-                    <CoreListItem_Left>
-                        <button className={startStateClasses} onClick={_ => StateManager.setNodeIsStart(nw1)} disabled={isStartNodeInternal}>
-                            {isStartNodeInternal ? 'Current Start State' : 'Set As Start State'}
-                        </button>
-                    </CoreListItem_Left>
-                </CoreListItem>
-            </div>}
-        </div>
-    );
+  let startStateClasses = `${isStartNodeInternal ? 'text-gray-700 dark:text-gray-300' : 'text-blue-500 dark:text-blue-400 '} flex flex-row items-center`
+  return (
+    <div className="flex flex-col">
+      <div className="font-medium text-2xl mb-2">State</div>
+      <div className="divide-y mb-3">
+        {!isMultiSelection && <ListItem title="Name" rightContent={nodeNameInput} />}
+        <ListItem title="Accepts" rightContent={nodeAcceptInput} />
+      </div>
+      {!isMultiSelection && <div className="divide-y mb-3">
+        <CoreListItem>
+          <CoreListItem_Left>
+            <button className={startStateClasses} onClick={_ => StateManager.setNodeIsStart(nw1)} disabled={isStartNodeInternal}>
+              {isStartNodeInternal ? 'Current Start State' : 'Set As Start State'}
+            </button>
+          </CoreListItem_Left>
+        </CoreListItem>
+      </div>}
+    </div>
+  );
 }
